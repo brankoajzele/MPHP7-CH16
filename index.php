bb3b5bdff4b23ec30994bbf53b94e59c7dc373c9
Branko Ajzele <ajzele@gmail.com>
2017-04-23 11:04:52 +0200

    Initial commit

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ use Foggyline\Catalog\Model\Product;
 use Foggyline\Catalog\Model\Category;
 use Foggyline\Catalog\Block\Category\View as CategoryView;
 
+$x = 'test-X';
+
 $category = new Category('Laptops');
+
+$y = 'test-Y';
 
 $category->addProduct(new Product('RL', 'Red Laptop', 1499.99, 25));
 $category->addProduct(new Product('YL', 'Yellow Laptop', 2499.99, 25));
@@ -16,4 +20,8 @@ $category->addProduct(new Product('BL', 'Blue Laptop', 3499.99, 25));
 
 $categoryView = new CategoryView($category);
 
+$z = 'test-Z';
+
 echo $categoryView->render();
+
+$q = 'test-Q';
